7423cfb5e475017ddbac747aa22f391588a53227
Joe Webb <webbdeveloping@gmail.com>
2020-03-06 20:45:55 -0700

    added react-router to the header

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,30 @@
 import React from 'react';
 import { ThemeProvider } from '@material-ui/styles';
-
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import theme from '../components/ui/Theme';
 import Header from './ui/Header';
 
 function App() {
   return (
-    <ThemeProvider theme={theme} >
-      <Header />
-      Hello!
+    <ThemeProvider theme={theme}>
+      <BrowserRouter>
+        <Header />
+        <Switch>
+          <Route exact path='/' component={() => <div>HOme</div>} />
+          <Route exact path='/services' component={() => <div>service</div>} />
+          <Route
+            exact
+            path='/customsoftware'
+            component={() => <div>custom</div>}
+          />
+          <Route exact path='/mobileapps' component={() => <div>apps</div>} />
+          <Route exact path='/websites' component={() => <div>web</div>} />
+          <Route exact path='/revolution' component={() => <div>rev</div>} />
+          <Route exact path='/about' component={() => <div>about</div>} />
+          <Route exact path='/contact' component={() => <div>contact</div>} />
+          <Route exact path='/estimate' component={() => <div>estimate</div>} />
+        </Switch>
+      </BrowserRouter>
     </ThemeProvider>
   );
 }
