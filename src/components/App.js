d406d4fdd76aebfd45ff31ef567a59e88d91f6cf
Joe Webb <webbdeveloping@gmail.com>
2020-03-07 19:14:23 -0700

    Added the basic footer set up

--- src/components/App.js
+++ src/components/App.js
@@ -3,6 +3,7 @@ import { ThemeProvider } from '@material-ui/styles';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import theme from '../components/ui/Theme';
 import Header from './ui/Header';
+import Footer from './ui/Footer';
 
 function App() {
   return (
@@ -10,20 +11,21 @@ function App() {
       <BrowserRouter>
         <Header />
         <Switch>
-          <Route exact path='/' component={() => <div>HOme</div>} />
+          <Route exact path='/' component={() => <div style={{height: '2000px'}}>Home</div>} />
           <Route exact path='/services' component={() => <div>service</div>} />
           <Route
             exact
             path='/customsoftware'
             component={() => <div>custom</div>}
           />
-          <Route exact path='/mobileapps' component={() => <div>apps</div>} />
+          <Route exact path='/mobileapps' component={() => <div >apps</div>} />
           <Route exact path='/websites' component={() => <div>web</div>} />
           <Route exact path='/revolution' component={() => <div>rev</div>} />
           <Route exact path='/about' component={() => <div>about</div>} />
           <Route exact path='/contact' component={() => <div>contact</div>} />
           <Route exact path='/estimate' component={() => <div>estimate</div>} />
         </Switch>
+        <Footer />
       </BrowserRouter>
     </ThemeProvider>
   );
