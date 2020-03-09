863f7baae7061759cd6f5d23581d11c009cfa4b1
Joe Webb <webbdeveloping@gmail.com>
2020-03-08 18:58:00 -0600

    finished services section

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -4,19 +4,21 @@ import { makeStyles, useTheme } from '@material-ui/core/styles';
 import Grid from '@material-ui/core/Grid';
 import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
+import useMediaQuery from '@material-ui/core/useMediaQuery';
 import ButtonArrow from '../components/ui/ButtonArrow';
 
 import animationData from '../animations/landinganimation/data';
+import customSoftwareIcon from '../assets/Custom Software Icon.svg';
 
 const useStyles = makeStyles(theme => ({
   mainContainer: {
-      marginTop: '5em',
-      [theme.breakpoints.down('md')]: {
-          marginTop: '3em'
-      },
-      [theme.breakpoints.down('xs')]: {
-          marginTop: '2em'
-      }
+    marginTop: '5em',
+    [theme.breakpoints.down('md')]: {
+      marginTop: '3em'
+    },
+    [theme.breakpoints.down('xs')]: {
+      marginTop: '2em'
+    }
   },
   animation: {
     maxWidth: '50em',
@@ -24,7 +26,7 @@ const useStyles = makeStyles(theme => ({
     marginTop: '2em',
     marginLeft: '10%',
     [theme.breakpoints.down('sm')]: {
-        maxWidth: '30em'
+      maxWidth: '30em'
     }
   },
   estimateButton: {
@@ -42,29 +44,52 @@ const useStyles = makeStyles(theme => ({
     marginTop: '1em'
   },
   learnButtonHero: {
-    borderColor: theme.palette.common.blue,
-    color: theme.palette.common.blue,
-    borderWidth: 2,
-    textTransform: 'none',
-    borderRadius: 50,
-    fontFamily: 'Roboto',
-    fontWeight: 'bold',
-    fonstSize: '0.9rem',
+    ...theme.typography.learnButton,
+    fontSize: '0.9rem',
     height: 45,
     width: 145
   },
-  heroTextContainer:{
-      minWidth: '21.5em',
-      marginLeft: '1em',
-      [theme.breakpoints.down('xs')]: {
-        marginLeft: 0
+  learnButton: {
+    ...theme.typography.learnButton,
+    fontSize: '0.7rem',
+    height: 35,
+    padding: 5,
+    [theme.breakpoints.down('sm')]:{
+        marginBottom: '2em'
     }
+  },
+  heroTextContainer: {
+    minWidth: '21.5em',
+    marginLeft: '1em',
+    [theme.breakpoints.down('xs')]: {
+      marginLeft: 0
+    }
+  },
+  specialText: {
+    fontFamily: 'Pacifico',
+    color: theme.palette.common.orange
+  },
+  subtitle: {
+    marginBottom: '1em'
+  },
+  icon: {
+    marginLeft: '2em',
+    [theme.breakpoints.down('xs')]: {
+      marginLeft: 0
+    }
+  },
+  serviceContainer: {
+    marginTop: '12em',
+    [theme.breakpoints.down('sm')]: {
+        padding: 25
+      }
   }
 }));
 
 const LandingPage = () => {
   const classes = useStyles();
   const theme = useTheme();
+  const matchesSM = useMediaQuery(theme.breakpoints.down("sm"))
 
   const defaultOptions = {
     loop: true,
@@ -78,6 +103,8 @@ const LandingPage = () => {
   return (
     <Grid container direction='column' className={classes.mainContainer}>
       <Grid item>
+        {' '}
+        {/* -----Hero Block----- */}
         <Grid container justify='flex-end' alignItems='center' direction='row'>
           <Grid sm item className={classes.heroTextContainer}>
             <Typography variant='h2' align='center'>
@@ -106,6 +133,33 @@ const LandingPage = () => {
           </Grid>
           <Grid sm item className={classes.animation}>
             <Lottie options={defaultOptions} height={'100%'} width={'100%'} />
+          </Grid>
+        </Grid>
+      </Grid>
+      <Grid item>
+        {' '}
+        {/* ---Services Block ----- */}
+        <Grid container direction='row' justify={matchesSM ? 'center' : undefined} className={classes.serviceContainer}>
+          <Grid item style={{ marginLeft: matchesSM ? 0 : '5em', textAlign: matchesSM ? "center" : undefined }}>
+            <Typography variant='h4'>Custom Software Development</Typography>
+            <Typography variant='subtitle1' className={classes.subtitle}>
+              Save Energy. Save Time. Save Money.
+            </Typography>
+            <Typography variant='subtitle1'>
+              Complete digital solutions, from investigation to{' '}
+              <span className={classes.specialText}>celebration.</span>
+            </Typography>
+            <Button variant='outlined' className={classes.learnButton}>
+              <span style={{ marginRight: 10 }}>Learn More</span>
+              &rarr;
+            </Button>
+          </Grid>
+          <Grid item>
+            <img
+              className={classes.icon}
+              alt='custom software icon'
+              src={customSoftwareIcon}
+            />
           </Grid>
         </Grid>
       </Grid>
--- src/components/ui/Theme.js
+++ src/components/ui/Theme.js
@@ -2,15 +2,16 @@ import { createMuiTheme } from '@material-ui/core/styles';
 
 const arcBlue = '#0B72B9';
 const arcOrange = '#ffba60';
+const arcGrey = '#868686'
 
 export default createMuiTheme({
   palette: {
     common: {
-      blue: `${arcBlue}`,
+      blue: arcBlue,
       orange: `${arcOrange}`
     },
     primary: {
-      main: `${arcBlue}`
+      main: arcBlue
     },
     secondary: {
       main: `${arcOrange}`
@@ -33,19 +34,33 @@ export default createMuiTheme({
       fontFamily: 'Raleway',
       fontWeight: 700,
       fontSize: '2.5rem',
-      color: `${arcBlue}`,
+      color: arcBlue,
       lineHeight: 1.5
     },
     h3: {
       fontFamily: 'Pacifico',
       fontSize: '2.5rem',
-      color: `${arcBlue}`
+      color: arcBlue
     },
     h4: {
       fontFamily: 'Raleway',
       fontSize: '1.75rem',
-      color: `${arcBlue}`,
+      color: arcBlue,
       fontWeight: 700
+    },
+    subtitle1: {
+      fontSize: '1.25rem',
+      fontWeight: 300,
+      color: arcGrey
+    },
+    learnButton: {
+      borderColor: arcBlue,
+      color: arcBlue,
+      borderWidth: 2,
+      textTransform: 'none',
+      borderRadius: 50,
+      fontFamily: 'Roboto',
+      fontWeight: 'bold',
     }
   }
 });
